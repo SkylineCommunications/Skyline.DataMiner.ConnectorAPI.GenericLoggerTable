133e8839efe28a3f7c196b7a5919f121bf387f1c
Thomas Remmery <thomas.remmery@skyline.be>
2023-12-13 15:44:06 +0100

    Defined classes representing results for every request.

---
+++ GenericLoggerTable/Messages/UpdateEntryResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Skyline.DataMiner.ConnectorAPI.GenericLoggerTable.Messages
+{
+    using Skyline.DataMiner.Core.InterAppCalls.Common.CallSingle;
+
+    public class UpdateEntryResult : Message
+    {
+        public bool Success { get; set; }
+
+        public string Reason { get; set; }
+    }
+}
