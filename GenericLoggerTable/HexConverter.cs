de6cf266f709c210d4f69b64c1c14f88fb9ab18a
Thomas Remmery <thomas.remmery@skyline.be>
2024-03-05 16:41:16 +0100

    Convert data to hexadecimal string before saving it to the database.

---
+++ GenericLoggerTable/HexConverter.cs
@@ -0,0 +1,56 @@
+﻿namespace Skyline.DataMiner.ConnectorAPI.GenericLoggerTable
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// Contains methods to convert Unicode strings to hexadecimal strings and vice versa.
+    /// </summary>
+    public static class HexConverter
+    {
+        /// <summary>
+        /// Converts a string to a hexadecimal string using Unicode encoding.
+        /// </summary>
+        /// <param name="str">String to convert to hexadecimal string.</param>
+        /// <returns>Hexadecimal string.</returns>
+        public static string ToHexString(string str)
+        {
+            if (String.IsNullOrEmpty(str)) return null;
+
+            var sb = new StringBuilder();
+            var bytes = Encoding.Unicode.GetBytes(str);
+            foreach (var t in bytes)
+            {
+                sb.Append(t.ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Converts a hexadecimal string to a Unicode encoded string.
+        /// </summary>
+        /// <param name="str">Hexadecimal string.</param>
+        /// <returns>Unicode string.</returns>
+        public static string FromHexString(string hexString)
+        {
+            if (String.IsNullOrEmpty(hexString)) return null;
+
+            var bytes = new byte[hexString.Length / 2];
+            try
+            {
+                for (var i = 0; i < bytes.Length; i++)
+                {
+                    bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+                }
+            }
+            catch (FormatException)
+            {
+                return hexString;
+            }
+
+
+            return Encoding.Unicode.GetString(bytes);
+        }
+    }
+}
